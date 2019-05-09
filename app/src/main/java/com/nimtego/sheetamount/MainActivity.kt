8f426e8be2f64f6115aabca67c5b43a531fbd27d
nimtego <nimtego@mail.ru>
2019-05-09 16:25:48 +0300

    ST-2[wip] Main screen imposition. fix base fragment

--- app/src/main/java/com/nimtego/sheetamount/MainActivity.kt
+++ app/src/main/java/com/nimtego/sheetamount/MainActivity.kt
@@ -8,5 +8,9 @@ class MainActivity : MvpAppCompatActivity(), MainView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.main_container, BaseAppFragment.getInstance("TEST"))
+            .commit()
     }
 }
